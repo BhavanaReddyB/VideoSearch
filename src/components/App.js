ded0171116db7566ab52b39918c083015c229dee
bbollu <bhavana_bollu@intuit.com>
2020-10-02 11:18:52 -0500

    Updated app to handle video requests from SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../components/apis/youtube';
 
 class App extends React.Component{
+    onTermSubmit = term => {
+        youtube.get("/search",{
+            params:{q: term}
+        });
+    }
+
     render(){
         return (
             // Adds gutter around the search bar on either side
             <div className="ui container"> 
-                <SearchBar/>
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
             </div>
         );
     }
